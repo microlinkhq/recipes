5a63cd96d6c95f9504afd228e41a95abed0626f5
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-06-30 19:55:49 +0200

    build: add index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  github: require('./github/github'),
+  hackerNews: require('./hacker-news/hacker-news'),
+  instagram: require('./instagram/instagram'),
+  meetup: require('./meetup/meetup'),
+  produchunt: require('./produchunt/produchunt'),
+  twitter: require('./twitter/twitter')
+}
