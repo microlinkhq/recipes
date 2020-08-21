83147ac9a2af6d8f7ab4c273ec7e8d78d326aa23
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-08-21 18:17:17 +0200

    fix: path

--- by-feature.js
+++ by-feature.js
@@ -1,5 +1,5 @@
 module.exports = {
-  fullyScreenshot: require('./recipes/by-feature/fullpage-screenshot'),
+  fullyScreenshot: require('./recipes/by-feature/fully-screenshot'),
   getHtml: require('./recipes/by-feature/get-html'),
   lighthouseReport: require('./recipes/by-feature/lighthouse-report'),
   technologyStack: require('./recipes/by-feature/technology-stack'),
