430f993939c1bdd6ea9d93adc72c67cd4cf119a2
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-11-30 14:49:14 +0100

    build: sort

--- by-feature.js
+++ by-feature.js
@@ -2,8 +2,8 @@ module.exports = {
   debugCss: require('./recipes/by-feature/debug-css'),
   fullyScreenshot: require('./recipes/by-feature/fully-screenshot'),
   getHtml: require('./recipes/by-feature/get-html'),
+  jsonLd: require('./recipes/by-feature/json-ld'),
   lighthouseReport: require('./recipes/by-feature/lighthouse-report'),
   technologyStack: require('./recipes/by-feature/technology-stack'),
-  universalEmbed: require('./recipes/by-feature/universal-embed'),
-  jsonLd: require('./recipes/by-feature/json-ld')
+  universalEmbed: require('./recipes/by-feature/universal-embed')
 }
