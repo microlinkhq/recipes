b0cf21b5883c29ef3322ef11d5bc57cc359e5333
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-07-18 12:07:13 +0200

    test: readd missing assertions

--- test/by-provider.js
+++ test/by-provider.js
@@ -4,21 +4,40 @@ const test = require('ava')
 const ow = require('ow')
 
 const {
-  pccomponentes,
-  hackerNews,
-  produchunt,
-  instagram,
+  betalist,
+  canopy,
+  codepen,
   coolmod,
-  twitter,
-  meetup,
+  fca,
   github,
+  google,
+  hackerNews,
+  imdb,
+  instagram,
+  meetup,
+  pccomponentes,
+  produchunt,
+  reddit,
+  telegram,
+  twitter,
   wipoid
 } = require('../by-provider')
 
 const apiKey = process.env.MICROLINK_API_KEY
 
+test('betalist', async t => {
+  const { data } = await betalist(betalist.meta.examples[0], { apiKey })
+
+  data.startups.forEach(startup => {
+    t.true(ow.isValid(startup.name, ow.string.not.empty))
+    t.true(ow.isValid(startup.url, ow.string.not.empty))
+    t.true(ow.isValid(startup.description, ow.string.not.empty))
+    t.true(ow.isValid(startup.image, ow.string.not.empty))
+  })
+})
+
 test('twitter', async t => {
-  const { data } = await twitter(twitter.info.examples[0], { apiKey })
+  const { data } = await twitter(twitter.meta.examples[0], { apiKey })
 
   t.true(ow.isValid(data.stats.followers, ow.number.finite))
   t.true(ow.isValid(data.stats.followings, ow.number.finite))
@@ -26,14 +45,14 @@ test('twitter', async t => {
 })
 
 test('produchunt', async t => {
-  const { data } = await produchunt(produchunt.info.examples[0], { apiKey })
+  const { data } = await produchunt(produchunt.meta.examples[0], { apiKey })
 
   t.true(ow.isValid(data.name, ow.string.not.empty))
   t.true(ow.isValid(data.upvotes, ow.string.not.empty))
 })
 
 test('pccomponentes', async t => {
-  const { data } = await pccomponentes(pccomponentes.info.examples[0], {
+  const { data } = await pccomponentes(pccomponentes.meta.examples[0], {
     apiKey
   })
 
@@ -42,14 +61,13 @@ test('pccomponentes', async t => {
 })
 
 test('wipoid', async t => {
-  const { data } = await wipoid(wipoid.info.examples[0], { apiKey })
+  const { data } = await wipoid(wipoid.meta.examples[0], { apiKey })
 
   t.true(ow.isValid(data.price, ow.number.finite))
 })
 
 test('meetup', async t => {
-  const { data } = await meetup(meetup.info.examples[0], { apiKey })
-
+  const { data } = await meetup(meetup.meta.examples[0], { apiKey })
   t.true(ow.isValid(data.members, ow.string.not.empty))
 
   data.pastEvents.forEach(post => {
@@ -75,7 +93,7 @@ test('hacker-news', async t => {
 })
 
 test('github', async t => {
-  const { data } = await github(github.info.examples[0], { apiKey })
+  const { data } = await github(github.meta.examples[0], { apiKey })
 
   t.true(ow.isValid(data.stats.followers, ow.number.finite))
   t.true(ow.isValid(data.stats.following, ow.number.finite))
@@ -83,8 +101,73 @@ test('github', async t => {
 })
 
 test('coolmod', async t => {
-  const { data } = await coolmod(coolmod.info.examples[0], { apiKey })
+  const { data } = await coolmod(coolmod.meta.examples[0], { apiKey })
 
   t.true(ow.isValid(data.price, ow.string.not.empty))
   t.true(ow.isValid(data.image, ow.object.not.empty))
+})
+
+test('google', async t => {
+  const { data } = await google(google.meta.examples[0], { apiKey })
+
+  data.results.forEach(result => {
+    t.true(ow.isValid(result.title, ow.string.not.empty))
+    t.true(ow.isValid(result.description, ow.string.not.empty))
+    t.true(ow.isValid(result.breadcumb, ow.string.not.empty))
+    t.true(ow.isValid(result.link, ow.string.not.empty))
+  })
+})
+
+test('imdb', async t => {
+  const { data } = await imdb(imdb.meta.examples[0], { apiKey })
+
+  t.true(ow.isValid(data.director, ow.string.not.empty))
+  t.true(ow.isValid(data.writer, ow.string.not.empty))
+  t.true(ow.isValid(data.duration, ow.string.not.empty))
+  t.true(ow.isValid(data.release, ow.string.not.empty))
+  t.true(ow.isValid(data.rating, ow.string.not.empty))
+  t.true(ow.isValid(data.ratingCount, ow.string.not.empty))
+})
+
+test('fca', async t => {
+  const { data } = await fca(fca.meta.examples[0], { apiKey })
+
+  t.true(ow.isValid(data.url, ow.string.not.empty))
+  t.true(ow.isValid(data.name, ow.string.not.empty))
+  t.true(ow.isValid(data.updatedAt, ow.string.not.empty))
+  t.true(ow.isValid(data.address, ow.string.not.empty))
+  t.true(ow.isValid(data.firmReferenceNumber, ow.number.finite))
+  t.true(ow.isValid(data.registeredCompanyNumber, ow.number.finite))
+  t.true(ow.isValid(data.email, ow.string.not.empty))
+  t.true(ow.isValid(data.phone, ow.string.not.empty))
+})
+
+test('canopy', async t => {
+  const { data } = await canopy(canopy.meta.examples[0], { apiKey })
+
+  data.products.forEach(result => {
+    t.true(ow.isValid(result.title, ow.string.not.empty))
+    t.true(ow.isValid(result.url, ow.string.not.empty))
+    t.true(ow.isValid(result.amazonUrl, ow.string.not.empty))
+    t.true(ow.isValid(result.image, ow.string.not.empty))
+  })
+})
+
+test('codepen', async t => {
+  const { data } = await codepen(codepen.meta.examples[0], { apiKey })
+  t.true(ow.isValid(data.screenshot, ow.object.not.empty))
+})
+
+test('telegram', async t => {
+  const { data } = await telegram(telegram.meta.examples[0], { apiKey })
+  t.true(ow.isValid(data.subscribers, ow.number.finite))
+  t.true(ow.isValid(data.photos, ow.number.finite))
+  t.true(ow.isValid(data.links, ow.string.not.empty))
+})
+
+test('reddit', async t => {
+  const { data } = await reddit(reddit.meta.examples[0], { apiKey })
+  t.true(ow.isValid(data.karma, ow.number.finite))
+  t.true(ow.isValid(data.birthday, ow.string.not.empty))
+  t.true(ow.isValid(data.avatar, ow.object.not.empty))
 })
